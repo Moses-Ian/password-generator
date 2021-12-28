6050265309696cf8be201463d6a7bdd39cd0cd50
Expired <imoses2@hotmail.com>
2021-12-28 11:27:49 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+This is a password generator. Simply click the button, answer the prompts, and your new password will appear in the text box! You are able to allow or disallow lowercase letters, uppercase letters, numbers, or special characters. You can choose a password length between 8 and 128 characters. If you don't like your password, simply click again and a new one will be generated!
+
+## Website
+
+You can access the deployed website here:
+<br>
+https://moses-ian.github.io/friendly-parakeet/
+
+### Screenshot
+
+![Screenshot](./assets/images/screenshot.png)
+
+## Credits
+
+* I followed the mock up from UofA Coding Bootcamp Challenge 3.
+
+## License
+
+Please do not copy any of my code.
+
+## Contributions
+
+Made by Ian Moses
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // Assignment code here
 var getPasswordLength = function() {
 	
-	//this needs to be validated
 	pl = window.prompt("Please enter a length for your password from 8 to 128 characters:");
 	pl = parseInt(pl);
 	
@@ -18,24 +17,13 @@ var generatePassword = function() {
 	do {
 		// prompt for character types
 		lc = window.confirm("Do you want lowercase characters?");
-		console.log("lc = " + lc);
-		
 		uc = window.confirm("Do you want uppercase characters?");
-		console.log("uc = " + uc);
-		
 		nc = window.confirm("Do you want numbers?");
-		console.log("nc = " + nc);
-		
 		sc = window.confirm("Do you want special characters?");
-		console.log("sc = " + sc);
-		
 	}while (!(lc || uc || nc || sc));	//VALIDATION - NO CHARACTER TYPES selected
 
-	console.log("valid character types");
-	
 	//prompt for password length
 	pl = getPasswordLength();
-	console.log("pl = " + pl);
 	
 	//put together the regex String
 	expr = "[";
@@ -44,8 +32,6 @@ var generatePassword = function() {
 	expr += nc ? "0-9" : "";
 	expr += sc ? "#%&():;<>@_~\!\$\'\*\+\,\-\.\/\=\?\[\\\]\^\`\|\~" : "";
 	expr += "]";
-	
-	console.log("expr = " + expr);
 	
 	//build the regex Object
 	expr = new RegExp(expr);
@@ -57,9 +43,7 @@ var generatePassword = function() {
 	
 		//generate a random character
 		rand = Math.floor(Math.random() * 128);
-		console.log("rand = " + rand);
 		chr = String.fromCharCode(rand);
-		console.log("chr = " + chr);
 		
 		//compare the character to the expression
 		result = expr.exec(chr);
@@ -67,47 +51,11 @@ var generatePassword = function() {
 			//if it's valid, add it
 			result = result.toString();
 			password += result;
-			console.log(password);
 		}
-		console.log(result);
 	}
 
 	return password;
 }
-
-var test = function() {
-	str = "[a-zA-Z0-9#%&():;<>@_~]";
-	var expr = new RegExp(str);
-	result = expr.exec("a");
-	if (result) {result = result.toString();}
-	console.log(result);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
